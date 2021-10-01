51837a0e65d97a6d69b8e61810ba73ce2868291f
brokencuph <415947405@qq.com>
2021-10-01 20:38:34 +0800

    construct CMake project structure

---
+++ study-to-think/cli_main.cpp
@@ -0,0 +1,12 @@
+﻿// study-to-think.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
