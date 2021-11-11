fb07e416e3af9c7d6898e4e0d4734cf4e52921ad
brokencuph <415947405@qq.com>
2021-11-11 12:46:11 +0800

    a tabular display of Student

--- study-to-think/mainwindow.cpp
+++ study-to-think/mainwindow.cpp
@@ -1,15 +1,55 @@
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QStandardItemModel>
+
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "db_access.h"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , currentDb(nullptr)
 {
     ui->setupUi(this);
+    
+    QObject::connect(ui->actionOpen, &QAction::triggered, this, &MainWindow::selectDbForOpen);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::selectDbForOpen(bool checked)
+{
+    try
+    {
+        auto fileName = QFileDialog::getOpenFileName(
+            this, tr("Open DB"), QString(), tr("SQLite DB File (*.db)"));
+        QMessageBox::information(this, "hahaha", fileName);
+        this->currentDb = std::make_unique<DbSession>(fileName.toStdString().c_str());
+        this->vStudent.clear();
+        this->currentDb->retrieveAll(this->vStudent);
+        QStandardItemModel* stuModel = new QStandardItemModel(this->vStudent.size(), 3);
+        for (int i = 0; i < this->vStudent.size(); i++)
+        {
+            stuModel->setItem(i, 0,
+                new QStandardItem(QString(vStudent[i].id.c_str())));
+            stuModel->setItem(i, 1,
+                new QStandardItem(QString(vStudent[i].name.c_str())));
+            stuModel->setItem(i, 2,
+                new QStandardItem(QString(vStudent[i].extraInfo.c_str())));
+        }
+        stuModel->setHeaderData(0, Qt::Horizontal, QString("Student ID"));
+        stuModel->setHeaderData(1, Qt::Horizontal, QString("Student Name"));
+        stuModel->setHeaderData(2, Qt::Horizontal, QString("Information"));
+        ui->tableStudent->setModel(stuModel);
+    }
+    catch (const std::exception& e)
+    {
+        QMessageBox::warning(this, tr("Error"), e.what());
+    }
 }
 
--- study-to-think/mainwindow.h
+++ study-to-think/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "db_access.h"
+#include "student.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    void selectDbForOpen(bool checked);
+
 private:
     Ui::MainWindow *ui;
+    std::unique_ptr<DbSession> currentDb;
+    std::vector<Student> vStudent;
 };
 #endif // MAINWINDOW_H
