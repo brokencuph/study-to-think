84852426b2215615a9ec55d8f6f3e55a8e6e21a3
brain_saver <zhangxiubo.must@gmail.com>
2021-11-30 12:00:06 +0800

    complete more function and now make it look like a piece of "not bad" thing But keep the bug last night

--- study-to-think/attendancescoredialog.cpp
+++ study-to-think/attendancescoredialog.cpp
@@ -10,35 +10,66 @@
 #include <QFormLayout>
 #include <QLineEdit>
 #include <QDialogButtonBox>
-int First_enter = 0;
-int Accept_switch = 0;
+#include<string>
+namespace attendanceNamespace {
+    int First_enter = 0;
+    int Accept_switch = 0;
+    int lessonNumber = 0;
+}
 AttendanceScoreDialog::AttendanceScoreDialog(QWidget *parent, const std::vector<Student>* _vStudent) :
     QDialog(parent),
     ui(new Ui::AttendanceScoreDialog)
 {
-    const std::vector<Student>& vStudent = *_vStudent;
-    if (!First_enter)
+    if (attendanceNamespace::Accept_switch) {
+        ui->setupUi(this);
+        QStringList attendanceFormList;
+        QStringList ClassIndexList;
+        attendanceFormList << "Normal" << "Late" << "Earlyleave" << "Absent";
+        ui->comboBox->addItems(attendanceFormList);
+        ui->comboBox->addItems(attendanceFormList);
+        for (int i = 1; i < attendanceNamespace::lessonNumber + 1; i++)
+        {
+            std::string addString = "lesson " + std::to_string(i);
+            QString qAddString = addString.c_str();
+            ClassIndexList << qAddString;
+
+        }
+
+        ui->comboBox_2->addItems(ClassIndexList);
+    }
+    if (!attendanceNamespace::First_enter)
     {
 
+        const std::vector<Student>& vStudent = *_vStudent;
         QDialog dialog(this);
         QFormLayout form(&dialog);
-        form.addRow(new QLabel("Please enter the number of class of this course:"));
+        form.addRow(new QLabel("Please enter the number of lessons of this course:"));
         QLineEdit* qlineedit1 = new QLineEdit(&dialog);
-        form.addRow( qlineedit1);
+        form.addRow(qlineedit1);
         QDialogButtonBox buttonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel,
             Qt::Horizontal, &dialog);
         form.addRow(&buttonBox);
         QObject::connect(&buttonBox, SIGNAL(accepted()), &dialog, SLOT(accept()));
         QObject::connect(&buttonBox, SIGNAL(rejected()), &dialog, SLOT(reject()));
-        Accept_switch = dialog.exec();
-        if (Accept_switch) {
-            Accept_switch = 1;
-            First_enter++; // will not enter next window until enter the number of class
+        attendanceNamespace::Accept_switch = dialog.exec();
+        if (attendanceNamespace::Accept_switch) {
+            attendanceNamespace::Accept_switch = 1;
+            attendanceNamespace::First_enter++; // will not enter next window until enter the number of class
             ui->setupUi(this);
             QStringList attendanceFormList;
             QStringList ClassIndexList;
             attendanceFormList << "Normal" << "Late" << "Earlyleave" << "Absent";
             ui->comboBox->addItems(attendanceFormList);
+            attendanceNamespace::lessonNumber = stoi(qlineedit1->text().toUtf8().toStdString());
+            for (int i = 1; i < attendanceNamespace::lessonNumber+1; i++)
+            {
+                std::string addString = "lesson " + std::to_string(i);
+                QString qAddString = addString.c_str();
+                ClassIndexList << qAddString;
+                
+            }
+            
+            ui->comboBox_2->addItems(ClassIndexList);
         }
         else
         {
@@ -47,13 +78,9 @@ AttendanceScoreDialog::AttendanceScoreDialog(QWidget *parent, const std::vector<
         }
         
     }
-    if (Accept_switch) {
-        ui->setupUi(this);
-        QStringList attendanceFormList;
-        QStringList ClassIndexList;
-        attendanceFormList << "Normal" << "Late" << "Earlyleave" << "Absent";
-        ui->comboBox->addItems(attendanceFormList);
-    }
+    
+    
+    
     
 }
 
