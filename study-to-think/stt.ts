cac674d4e5b4e564eabcfbd5f131ab5b51720155
brokencuph <415947405@qq.com>
2021-12-05 23:33:09 +0800

    add translation capability

---
+++ study-to-think/stt.ts
@@ -0,0 +1,226 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AttendanceScoreDialog</name>
+    <message>
+        <location filename="attendancescoredialog.ui" line="14"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_attendancescoredialog.h" line="46"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="226"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="241"/>
+        <source>Student</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="232"/>
+        <source>Filter...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="64"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="234"/>
+        <source>Add a Student</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <location filename="mainwindow.ui" line="122"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="236"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="242"/>
+        <source>+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="85"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="238"/>
+        <source>Remove Current Student</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="88"/>
+        <location filename="mainwindow.ui" line="140"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="240"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="243"/>
+        <source>-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="99"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="244"/>
+        <source>Scheme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="151"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="245"/>
+        <source>Overview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="183"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="246"/>
+        <source>&amp;File</source>
+        <translation>文件(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="197"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="227"/>
+        <source>&amp;Open...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="202"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="228"/>
+        <source>&amp;New...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="207"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="229"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="215"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="230"/>
+        <source>&amp;Import...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="223"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_mainwindow.h" line="231"/>
+        <source>&amp;Export...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="70"/>
+        <source>Open DB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="70"/>
+        <location filename="mainwindow.cpp" line="90"/>
+        <source>SQLite DB File (*.db)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="81"/>
+        <location filename="mainwindow.cpp" line="104"/>
+        <location filename="mainwindow.cpp" line="135"/>
+        <location filename="mainwindow.cpp" line="169"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="90"/>
+        <source>Choose DB File Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="123"/>
+        <location filename="mainwindow.cpp" line="156"/>
+        <source>Choose a folder to save...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="131"/>
+        <source>Success</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="131"/>
+        <source>Export successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="217"/>
+        <source>Student ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="218"/>
+        <source>Student Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="219"/>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="220"/>
+        <location filename="mainwindow.cpp" line="547"/>
+        <source>Total Score</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="464"/>
+        <location filename="mainwindow.cpp" line="476"/>
+        <source>Invalid Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="464"/>
+        <location filename="mainwindow.cpp" line="476"/>
+        <source>Input could not contain single quote.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="605"/>
+        <source>Average Score:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ManualScoreDialog</name>
+    <message>
+        <location filename="manualscoredialog.ui" line="14"/>
+        <location filename="../build/study-to-think/stt_gui_autogen/include/ui_manualscoredialog.h" line="47"/>
+        <source>Edit Student Scores (Manual)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="manualscoredialog.cpp" line="61"/>
+        <location filename="manualscoredialog.cpp" line="68"/>
+        <source>Invalid Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="manualscoredialog.cpp" line="61"/>
+        <source>Input could not contain non-digit.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="manualscoredialog.cpp" line="68"/>
+        <source>Score must be in [0,100].</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="mainwindow.cpp" line="34"/>
+        <source>Distribution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="35"/>
+        <source>Statistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
