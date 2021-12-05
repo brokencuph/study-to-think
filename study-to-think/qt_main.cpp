97ed1eca67fbaf754e288ecda634d5e2eacd93b2
brokencuph <415947405@qq.com>
2021-12-05 17:11:54 +0800

    add qt theme (only for fun)

--- study-to-think/qt_main.cpp
+++ study-to-think/qt_main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QFile file("ExcelOffice.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet{ QLatin1String(file.readAll()) };
+
+    a.setStyleSheet(styleSheet);
+
     w.show();
     return a.exec();
 }
